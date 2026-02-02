28d9fbe99a9279a96172fea56401aab4870f16c4
dongsukim1 <123356268+dongsukim1@users.noreply.github.com>
2026-02-02 12:01:14 -0800

    attempting recalibration

--- ONNX_diagnostics.py
+++ ONNX_diagnostics.py
@@ -24,7 +24,8 @@ def load_torch_model(pth_path, num_classes=6):
 
 def preprocess_image(image_path):
     transform = transforms.Compose([
-        transforms.Resize((224, 224)),
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     ])
--- calibrate_onnx.py
+++ calibrate_onnx.py
@@ -22,7 +22,8 @@ class CalibrationReader(CalibrationDataReader):
         self.batch_size = batch_size
         self.image_paths = []
         self.transform = transforms.Compose([
-            transforms.Resize((224, 224)),
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
             transforms.ToTensor(),
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
         ])
